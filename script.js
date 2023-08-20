d6073b67a82b8cc9ba8d22d60c4cf10a8506b877
Deepak-Wagh-Project <133074729+Deepak-Wagh-Project@users.noreply.github.com>
2023-08-20 17:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const ele= document.getElementByID("output");
+const input= document.getElementById("text");
+const time = document.getElementById("delay");
+async function displayText(){
+	let val=time.value.trim();
+	setTimeOut(
+		()=>{
+			ele.innerText=input.value;
+		},val*1000);
+	
+}
